429d2ea04ff17a004cab7aac5eb1a66e583e8ee0
joel912 <joelhehehe1234@gmail.com>
2025-05-26 10:59:07 +0530

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int reverse = 0;
+        while (number > 0){
+            int digit = number %10;
+            reverse = reverse*10 + digit;
+            number = number/10;
+        }
+        System.out.println(number);
     }
 }
