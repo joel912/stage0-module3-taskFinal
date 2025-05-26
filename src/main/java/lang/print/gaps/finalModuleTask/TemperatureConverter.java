b9b95ffcd49dae42e86bc2415685974b5629eb3e
joel912 <joelhehehe1234@gmail.com>
2025-05-26 11:04:16 +0530

    Update TemperatureConverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        double fahrenheit = temperatureCelsius * 9.0 / 5 + 32;
+        System.out.println(fahrenheit);
     }
 }
